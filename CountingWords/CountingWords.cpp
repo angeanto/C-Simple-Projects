75dcebb55b5866b1f327d6d9feb540ae633d147d
angeanto <31348777+angeanto@users.noreply.github.com>
2017-09-06 17:54:10 +0300

    Add files via upload

---
+++ CountingWords/CountingWords.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include <iostream>
+#include<sstream>
+using namespace std; 
+using std::endl; 
+
+int main()
+{
+
+	string line, word;
+	getline(cin, line);
+	stringstream ss(line);
+	int word_count = 0;
+	while (getline(ss, word, ' ')) word_count += word.length() ? 1 : 0;
+
+	cout << "Number of words= " << word_count<< endl; 
+
+
+
+	cin.get();
+    return 0;
+}
---
+++ airSeats/airSeatsREADME.md
@@ -0,0 +1,6 @@
+# C-plusplus-Simple-Projects
+Simple projects using C++ 
+
+This program displays seats of an airplane and asks you to choose a seat for your ticket. 
+It marks a reserved seat with 'X'
+Shows possible invalid rows or letters of seats
