c34829994a37df0981346c4210db38b70e6bebc5
Antonios Angelakis <31348777+angeanto@users.noreply.github.com>
2017-09-07 11:29:16 +0300

    Update airSeats.cpp

--- airSeats/airSeats.cpp
+++ airSeats/airSeats.cpp
@@ -6,14 +6,14 @@ using namespace std;
 int main()
 {
 	int row , column ;
-	int matrix1[7][1];
+	int matrix1[7][1]; // matrix1 vertically displays number of each row
 
 	for (row = 0; row < 7; row++)
 	{
-		matrix1[row][0] = row + 1;
+		matrix1[row][0] = row + 1; // fill matrix1 with elements >=1
 	}
 	char matrix2[7][5];
-	for (row = 0; row < 7; row++)
+	for (row = 0; row < 7; row++) // matrix2 displays letters for each seat in this way "AB CD" 
 	{
 		matrix2[row][0] = 'A';
 		matrix2[row][1] = 'B';
@@ -26,34 +26,34 @@ int main()
 	{
 		cout << row + 1 << " ";
 		for (column = 0; column < 5; column++)
-			cout <<matrix2[row][column];
+			cout <<matrix2[row][column]; // Get the overall seat design for the airplane 
 		    cout << endl;
 	}
 	
 	int full = 0;
 
-	while (full != 28)
+	while (full != 28)                  // 28 is the number of available seats , program ends when there are no available seats
 	{
 		int row ;
 		cout << "Choose row" << endl;
 		cin >> row;
-		if (row > 7)
+		if (row > 7) 
 		{
 			cout << "Invalid number" << endl;
-			continue;
+			continue;                //asks us to give again a valid number of row
 		}
 		
 		cout << "Choose column" << endl;
-		cin >> matrix2[row][column];
+		cin >> matrix2[row][column];        // only A,B,C,D are valid inputs
 		if ((matrix2[row][column] != 'A') && (matrix2[row][column] != 'B') && (matrix2[row][column] != 'C') && (matrix2[row][column] != 'D'))
-		{
+		{ 
 			cout << " Invalid column" << endl;
 			continue;
 		}
 		switch (matrix2[row][column])
 		{
 		case 'A':
-			matrix2[row-1][0] = 'X';
+			matrix2[row-1][0] = 'X';  // row-1 because matrix counts from zero 
 			full = full + 1;
 			break;
 		case  'B':
@@ -74,11 +74,11 @@ int main()
 		{
 			cout << row + 1 << " ";
 			for (column = 0; column < 5; column++)
-				cout << matrix2[row][column];
+				cout << matrix2[row][column]; // outputs current available seats 
 			cout << endl;
 		}
 
-		cout << 28 - full << "Remaining seats" << endl;
+		cout << 28 - full << "Remaining seats" << endl; // full increases when we input a valid seat
 
 	}
 	if (full == 28)
